e63bb119d0a764843fa50760f9ee73d045bfde6e
DABIN JEONG <41@b41.kr>
2022-04-03 02:55:39 +0900

    Modify - Theme Type 특정 값으로 고정

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,41 +3,41 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     color: {
-      primaryDeepBlue: string;
-      primaryBlue: string;
-      navblue: string;
-      black: string;
-      white: string;
-      gray: string;
-      borderGray: string;
-      orange: string;
+      primaryDeepBlue: '#1565C0';
+      primaryBlue: '#2196F3';
+      navblue: '#1565C0';
+      black: '#323D45';
+      white: '#fff';
+      gray: '#939FA5';
+      borderGray: '#E5E5E5';
+      orange: '#FFA000';
     };
 
     fontSize: {
-      xs: string;
-      s: string;
-      m: string;
-      l: string;
-      xl: string;
+      xs: '12px';
+      s: '14px';
+      m: '16px';
+      l: '20px';
+      xl: '24px';
     };
 
     fontWeight: {
-      strong: number;
-      semiStrong: number;
-      normal: number;
+      strong: 800;
+      semiStrong: 500;
+      normal: 400;
     };
 
     lineHeight: {
-      s: number;
-      m: number;
-      l: number;
+      s: 1.42;
+      m: 1.5;
+      l: 1.6;
     };
 
     breakpoints: {
-      mobile: string;
-      tablet: string;
-      laptop: string;
-      desktop: string;
+      mobile: '@media screen and (max-width: 480px)';
+      tablet: '@media screen and (min-width: 481px)';
+      laptop: '@media screen and (min-width: 769px)';
+      desktop: '@media screen and (min-width: 1025px)';
     };
   }
 }
